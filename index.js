d17c37fecf486ec57ccf1e8fc8fb2ec2b6d7623f
Adesh Shah <shah0150@algonquinlive.com>
2018-04-11 16:28:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 let app = {
-
-    
-
-    init: function(){
+    baseURL: 'YOUR-OWN-URL',
+    device_id: null,
+    token: null,
+    pages: [],
+    currentPerson: 0,
+    init: function() {
         app.connect();
     },
-
-    connect: function(){
-
+    
+    connect: function() {
+        // create a session for the current app user
+        if('device' in window){  
+            app.device = device.uuid;
+            console.log(app.device);
+        }else{
+            app.device = '1234567890abcdef';
+        }
+        let url = `${app.baseURL}connect/?device_id=${app.device}`;
+        console.log(url);
+        fetch(url)
+        .then(function(response){
+            if (response.ok) {
+                return response.json();
+              } else {
+                throw new Error('Something went wrong');
+              }
+        })
+        .then(function(data){
+            if(data.code==0){
+                //save the token to be used later
+                app.token = data.token;
+                console.log("Token is ", app.token);
+                //get the people
+                // app.getPeople();
+            }else{
+               console.log("error") 
+            }
+        })
+        .catch(function(err){
+            console.log("Error ", err);
+        });
     }
 }
 
